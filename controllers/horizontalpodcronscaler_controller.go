fb9139e916fab337b9835a83d8aefbc99511d78f
Kenji Shimizu <jrywm121@gmail.com>
2021-08-03 01:58:21 +0900

    🚧 change reconciler's structure

--- controllers/horizontalpodcronscaler_controller.go
+++ controllers/horizontalpodcronscaler_controller.go
@@ -18,20 +18,43 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-logr/logr"
+	cronv3 "github.com/robfig/cron/v3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	crlog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	autoscalingv1beta1 "github.com/44smkn/horizontal-pod-cronscaler/api/v1beta1"
-	"github.com/44smkn/horizontal-pod-cronscaler/pkg/scaling"
 )
+
+type namespacedName = string
 
 // HorizontalPodCronscalerReconciler reconciles a HorizontalPodCronscaler object
 type HorizontalPodCronscalerReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme      *runtime.Scheme
+	cron        *cronv3.Cron
+	cronMapping map[namespacedName]cronv3.EntryID
+	ch          chan *Schedule
+}
+
+func NewHorizontalPodCronscalerReconciler(client client.Client, scheme *runtime.Scheme) *HorizontalPodCronscalerReconciler {
+	cron := cronv3.New()
+	go cron.Start()
+	return &HorizontalPodCronscalerReconciler{
+		Client:      client,
+		Scheme:      scheme,
+		cron:        cron,
+		cronMapping: make(map[string]cronv3.EntryID),
+		ch:          make(chan *Schedule),
+	}
 }
 
 //+kubebuilder:rbac:groups=autoscaling.44smkn.github.io,resources=horizontalpodcronscalers,verbs=get;list;watch;create;update;patch;delete
@@ -42,26 +65,79 @@ type HorizontalPodCronscalerReconciler struct {
 func (r *HorizontalPodCronscalerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := crlog.FromContext(ctx)
 
+	log.Info("start reconcile")
 	hpc := &autoscalingv1beta1.HorizontalPodCronscaler{}
 	if err := r.Get(ctx, req.NamespacedName, hpc); err != nil {
 		log.Error(err, "unable to fetch HorizontalPodCronscaler")
 		return ctrl.Result{}, err
 	}
 
-	if !hpc.DeletionTimestamp.IsZero() {
-		scaling.RemoveSchedule(req.NamespacedName.String())
-		log.Info("remove schedule")
-		return ctrl.Result{}, nil
-	}
+	name := fmt.Sprintf("%s/%s", hpc.Namespace, hpc.Name)
+	sched := &Schedule{
+		name:           name,
+		cronExpression: hpc.Spec.Schedule,
+		shouldDelete:   !hpc.DeletionTimestamp.IsZero(),
+		fn: func() {
+			log.Info(fmt.Sprintf("scale %s replicas to %v", name, hpc.Spec.Replicas))
 
-	scaling.AddSchedule(ctx, hpc, r.Client, log)
+			patch := &unstructured.Unstructured{}
+			patch.SetGroupVersionKind(hpc.GroupVersionKind())
+			patch.SetNamespace(hpc.Namespace)
+			patch.SetName(hpc.Name)
+
+			patch.UnstructuredContent()["spec"] = map[string]interface{}{
+				"replicas": hpc.Spec.Replicas,
+			}
+			err := r.Client.Patch(ctx, patch, client.Apply, &client.PatchOptions{
+				FieldManager: "horizontal-pod-cronscaler",
+				Force:        pointer.BoolPtr(true),
+			})
+			if err != nil {
+				log.Error(err, "failed to patch")
+			}
+
+			hpc.Status = autoscalingv1beta1.HorizontalPodCronscalerStatus{
+				LastTargetReplicas: &hpc.Spec.Replicas,
+				LastSchedule:       metav1.NewTime(time.Now()),
+			}
+
+			if err != nil {
+				log.Error(err, "failed to addFunc")
+			}
+		},
+	}
+	log.Info("before send message to channel")
+	r.ch <- sched
 	log.Info("added schedule", "schedule", hpc.Spec.Schedule)
 
 	return ctrl.Result{}, nil
 }
 
+type Schedule struct {
+	name           namespacedName
+	cronExpression string
+	fn             func()
+	shouldDelete   bool
+}
+
+func (r *HorizontalPodCronscalerReconciler) putSchedule(ch <-chan *Schedule, log logr.Logger) {
+	for p := range ch {
+		if id, ok := r.cronMapping[p.name]; ok {
+			r.cron.Remove(id)
+		}
+		if !p.shouldDelete {
+			id, err := r.cron.AddFunc(p.cronExpression, p.fn)
+			if err != nil {
+				log.Error(err, "failed to add func")
+			}
+			r.cronMapping[p.name] = id
+		}
+	}
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *HorizontalPodCronscalerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	go r.putSchedule(r.ch, crlog.Log)
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&autoscalingv1beta1.HorizontalPodCronscaler{}).
 		Complete(r)
